622c16a4d91e40b8d219b75c82731c464bfa3034
honjoh <honjoh@logix.jp>
2021-02-18 22:59:52 +0800

    Revert "no message"
    This reverts commit 8437cc88fce78fdc496e803a6014dd19f51d9071.

--- src/components/tags.tsx
+++
@@ -1,9 +0,0 @@
-return tags.map(tag => {
-    return (
-      <span className="mr4" key={tag}>
-         <Link href={`/tags/[tagName]`} as={`/tags/${tag}`} passHref prefetch={false}>
-            <a className={tagStyles.tagLink}>{tag}</a>
-         </Link>
-      </span>
-    )
-  })

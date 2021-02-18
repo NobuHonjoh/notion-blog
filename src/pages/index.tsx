4ed6af0723ad11fbf2f32f711a0ef34a1752c825
honjoh <honjoh@logix.jp>
2021-02-18 23:22:12 +0800

    features不要

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
+import Features from '../components/features'
 import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 
@@ -21,7 +22,7 @@ export default () => (
         </ExtLink>
       </h2>
 
-      <Features />
+      {/* <Features /> */}
 
       <div className="explanation">
         <p>
